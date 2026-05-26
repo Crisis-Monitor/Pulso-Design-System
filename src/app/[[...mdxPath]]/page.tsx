a3b78f798a84d2ab8ac43aabe2bd1453a8af44c1
Neviton Santana <neviton.santana@dotcase.com.br>
2026-05-26 00:41:50 -0300

    fix(docs): render Nextra page wrapper

--- src/app/[[...mdxPath]]/page.tsx
+++ src/app/[[...mdxPath]]/page.tsx
@@ -5,6 +5,14 @@ export const generateStaticParams = generateStaticParamsFor('mdxPath');
 
 export default async function Page(props: { params: Promise<{ mdxPath?: string[] }> }) {
   const params = await props.params;
-  const { default: MDXContent } = await importPage(params.mdxPath);
-  return <MDXContent components={useMDXComponents({})} />;
+  const result = await importPage(params.mdxPath);
+  const { default: MDXContent, metadata, sourceCode, toc } = result;
+  const components = useMDXComponents({});
+  const Wrapper = components.wrapper;
+
+  return (
+    <Wrapper metadata={metadata} sourceCode={sourceCode} toc={toc}>
+      <MDXContent components={components} params={params} />
+    </Wrapper>
+  );
 }
