394b6d76c01e84abe2e538c84f739e0c12dc32b5
Neviton Santana <neviton.santana@dotcase.com.br>
2026-05-25 23:25:55 -0300

    docs(data-viz): add Pulso chart foundations

---
+++ content/data-visualization/_meta.ts
@@ -0,0 +1,9 @@
+export default {
+  'data-color': 'Data color',
+  primitives: 'Chart primitives',
+  axes: 'Axes & tooltips',
+  realtime: 'Real-time motion',
+  recipes: 'Chart recipes',
+  'score-ring': 'Score Ring',
+  'sentiment-bar': 'Sentiment Bar',
+}
