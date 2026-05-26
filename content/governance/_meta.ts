19534a29a1f1ab04ec238863bc28e6d26febf193
Neviton Santana <neviton.santana@dotcase.com.br>
2026-05-25 23:31:53 -0300

    docs(governance): add token lifecycle foundations

---
+++ content/governance/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+  'token-architecture': 'Token architecture',
+  'figma-parity': 'Figma ↔ code parity',
+  'rfc-process': 'RFC process',
+  'adoption-metrics': 'Adoption metrics',
+  'states-matrix': 'States matrix',
+}
